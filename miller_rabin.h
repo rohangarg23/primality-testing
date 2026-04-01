b0c70d1d24ca09b46a2e2ce9815a9e8baa2cbcb3
Rohan Garg <152214935+rohangarg23@users.noreply.github.com>
2026-04-01 17:27:53 +0530

    Declare probable prime generation helper

--- miller_rabin.h
+++ miller_rabin.h
@@ -1,11 +1,14 @@
 #ifndef MILLER_RABIN_H
 #define MILLER_RABIN_H
 
+#include <cstddef>
+
 #include "big_int.h"
 
 big_int modular_multiply_ordinary(const big_int& a, const big_int& b, const big_int& mod);
 big_int modular_multiply_fast(const big_int& a, const big_int& b, const big_int& mod);
 big_int modular_power(big_int base, big_int exponent, const big_int& mod, bool use_fast_mod);
 bool miller_rabin(const big_int& n, bool use_fast_mod);
+big_int generate_probable_prime(std::size_t bits);
 
 #endif
